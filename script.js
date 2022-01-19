e904334deac7ff2f75c0b324d0bac724e349e614
manikagnish <72827130+manikagnish@users.noreply.github.com>
2022-01-19 16:18:43 +0530

    added basic ball functionality

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-"use strict";
+import Ball from "./Ball.js";
+
+const ball = new Ball(document.getElementById("ball"));
+
+let lastTime;
+function update(time) {
+  if (lastTime != null) {
+    const delta = time - lastTime;
+    ball.update(delta);
+  }
+
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+
+window.requestAnimationFrame(update);
